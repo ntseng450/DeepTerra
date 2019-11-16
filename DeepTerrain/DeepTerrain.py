78a9662385c6793be56dc7c9bb19a503005da139
ntseng450 <nathan.tseng.3.14159@gmail.com>
2019-11-16 11:43:46 -0500

    Fixed so training converges

--- DeepTerrain/DeepTerrain.py
+++ DeepTerrain/DeepTerrain.py
@@ -9,7 +9,7 @@ from keras.callbacks import LearningRateScheduler, TerminateOnNaN
 from keras.optimizers import Adam, SGD
 import os
 
-batch_size = 4
+batch_size = 12
 num_classes = 3
 epochs = 120
 data_augmentation = True
@@ -18,43 +18,62 @@ save_dir = os.path.join(os.getcwd(), 'saved_models')
 model_name = 'keras_cifar10_trained_model.h5'
 
 
-datagen = ImageDataGenerator( featurewise_center=False, samplewise_center=False,
-    featurewise_std_normalization=False, samplewise_std_normalization=False, zca_whitening=False,
-    zca_epsilon=1e-06, rotation_range=0, width_shift_range=0.1, height_shift_range=0.1,
-    shear_range=0., zoom_range=0., channel_shift_range=0., fill_mode='nearest',
-    cval=0., horizontal_flip=True, vertical_flip=False, rescale=None,
-    preprocessing_function=None, data_format=None, validation_split=0.2)
+datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
 
-train_it = datagen.flow_from_directory('RoverImageSet/train/', target_size = (150,150), class_mode = 'categorical', batch_size = batch_size)
-val_it = datagen.flow_from_directory('RoverImageSet/val/', target_size = (150,150), class_mode = 'categorical', batch_size = 2)
+train_it = datagen.flow_from_directory(
+    directory = 'RoverImageSet/train/',
+    target_size = (160,160),
+    color_mode = 'rgb',
+    batch_size = batch_size,
+    class_mode = 'categorical',
+    shuffle = True,
+    seed = 42)
+
+val_it = datagen.flow_from_directory(
+    directory = 'RoverImageSet/val/',
+    target_size = (160,160),
+    color_mode = 'rgb',
+    batch_size = 4,
+    class_mode = 'categorical',
+    shuffle = True,
+    seed = 42)
+
+train_steps = train_it.n//train_it.batch_size
+val_steps = val_it.n//val_it.batch_size
+labels = (train_it.class_indices)
+for category_label in labels:
+    print(category_label)
+input_shape = (160, 160,3)
 
 model = Sequential()
-model.add(Conv2D(32, (3, 3), padding='same',
-                 input_shape = (150,150, 3)))
+model.add(Conv2D(32, (3, 3), input_shape=input_shape))
 model.add(Activation('relu'))
 model.add(Conv2D(32, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
 model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
 
 model.add(Flatten())
 model.add(Dense(100))
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
-model.add(Dense(3))
+model.add(Dense(2))
 model.add(Activation('softmax'))
 
 adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
 
 # Let's train the model using RMSprop
 model.compile(loss='categorical_crossentropy',
-              optimizer=adam,
+              optimizer='rmsprop',
               metrics=['accuracy'])
 
 def lr_schedule(epoch):
@@ -66,14 +85,15 @@ def lr_schedule(epoch):
         return 0.00001
 
 learning_rate_scheduler = LearningRateScheduler(schedule=lr_schedule,
-                                                verbose=1)
+            verbose = 1)
 
 terminate_on_nan = TerminateOnNaN()
 
 callbacks = [learning_rate_scheduler,
              terminate_on_nan]
 
-model.fit_generator(train_it, steps_per_epoch = 10, epochs = epochs, callbacks = callbacks, validation_data = val_it, validation_steps = 5)
+model.fit_generator(generator = train_it, steps_per_epoch = train_steps,
+                    epochs = epochs, callbacks = callbacks, validation_data = val_it, validation_steps = val_steps)
 # Save model and weights
 if not os.path.isdir(save_dir):
     os.makedirs(save_dir)
